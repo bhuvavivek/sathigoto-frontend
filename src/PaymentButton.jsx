50707fa3795b4a81446642db340676860abc6cb4
bhuvavivek <bhuvavivek67@gmaill.com>
2025-02-24 16:36:37 +0530

    try catch added

--- src/PaymentButton.jsx
+++ src/PaymentButton.jsx
@@ -10,55 +10,60 @@ const PaymentButton = () => {
     const { isLoading, Razorpay } = useRazorpay();
 
     const handlePayment = async () => {
-        const response = await fetch(`${APiURL}/create-order`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-        })
-        const data = await response.json()
+        try {
+            const response = await fetch(`${APiURL}/create-order`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            const data = await response.json()
 
-        const options = {
-            key: import.meta.env.VITE_API_KEY,
-            amount: data.amount, // Amount in paise
-            currency: "INR",
-            name: "SathiGoto",
-            description: "Test Transaction",
-            // image:'',
-            order_id: data.order_id, // Generate order_id on server
-            handler: (response) => {
-                fetch(`${APiURL}/verify-payment`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        razorpay_order_id: response.razorpay_order_id,
-                        razorpay_payment_id: response.razorpay_payment_id,
-                        razorpay_signature: response.razorpay_signature
-                    })
-                }).then(res => res.json())
-                    .then(data => {
-                        if (data.status === 'ok') {
-                            setIsPaymentSuccessful(true)
-                            window.location.href = '/payment-success';
-                        } else {
-                            setIsPaymentSuccessful(false)
-                            alert('Payment verification failed');
-                        }
-                    }).catch(error => {
-                        console.error('Error:', error);
-                        alert('Error verifying payment');
-                    });
-            },
+            const options = {
+                key: import.meta.env.VITE_API_KEY,
+                amount: data.amount, // Amount in paise
+                currency: "INR",
+                name: "SathiGoto",
+                description: "Test Transaction",
+                // image:'',
+                order_id: data.order_id, // Generate order_id on server
+                handler: (response) => {
+                    fetch(`${APiURL}/verify-payment`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            razorpay_order_id: response.razorpay_order_id,
+                            razorpay_payment_id: response.razorpay_payment_id,
+                            razorpay_signature: response.razorpay_signature
+                        })
+                    }).then(res => res.json())
+                        .then(data => {
+                            if (data.status === 'ok') {
+                                setIsPaymentSuccessful(true)
+                                window.location.href = '/payment-success';
+                            } else {
+                                setIsPaymentSuccessful(false)
+                                alert('Payment verification failed');
+                            }
+                        }).catch(error => {
+                            console.error('Error:', error);
+                            alert('Error verifying payment');
+                        });
+                },
 
-            theme: {
-                color: "#F37254",
-            },
+                theme: {
+                    color: "#F37254",
+                },
+            }
+
+            const razorpayInstance = new Razorpay(options);
+            razorpayInstance.open();
+        } catch (error) {
+            console.log(error)
         }
 
-        const razorpayInstance = new Razorpay(options);
-        razorpayInstance.open();
     };
 
     return (
